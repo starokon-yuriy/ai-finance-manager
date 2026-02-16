f91a74c8470c39485362c0d1bcbf8a933717d478
Yurii Starokon <yuriist@ext.inditex.com>
2026-02-16 15:22:32 +0200

    init Spring boot

---
+++ ai-finance-manager/src/main/java/com/ys/ai/aifinancemanager/AiFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ys.ai.aifinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFinanceManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiFinanceManagerApplication.class, args);
+  }
+
+}
