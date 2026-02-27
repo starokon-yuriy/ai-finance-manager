c913b7990bd5647df8fa78f077fc350f99e113bd
Yurii Starokon <yuriist@ext.inditex.com>
2026-02-27 21:11:25 +0200

    fix tests

--- ai-finance-manager/src/test/java/com/ys/ai/aifinancemanager/AiFinanceManagerApplicationTests.java
+++ ai-finance-manager/src/test/java/com/ys/ai/aifinancemanager/AiFinanceManagerApplicationTests.java
@@ -2,8 +2,10 @@ package com.ys.ai.aifinancemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AiFinanceManagerApplicationTests {
 
   @Test
